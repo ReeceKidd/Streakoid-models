8f22a74fdd2e6caf9102967ebe402f8982ef6d30
Reece Kidd <reecekidd95@gmail.com>
2020-04-30 16:03:40 +0100

    chore: Added lib to gitignore

--- lib/Models/PopulatedUser.d.ts
+++ lib/Models/PopulatedUser.d.ts
@@ -10,7 +10,7 @@ export interface PopulatedUser {
     timezone: string;
     followers: BasicUser[];
     following: BasicUser[];
-    totalStreakCompleted: number;
+    totalStreakCompletes: number;
     achievements: DatabaseAchievementType[];
     createdAt: string;
     updatedAt: string;
