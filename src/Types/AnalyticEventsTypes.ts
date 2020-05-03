61c3f78e926b4ac89696c8637d41a63afc0d91b7
Reece Kidd <reecekidd95@gmail.com>
2020-05-03 12:27:24 +0100

    refactor: Updated AnalyticEventsTypes for readability

--- src/Types/AnalyticEventsTypes.ts
+++ src/Types/AnalyticEventsTypes.ts
@@ -1,14 +1,14 @@
 enum AnalyticEventsTypes {
-    registered = 'registered',
-    completedSoloStreak = 'completedSoloStreak',
-    completedChallengeStreak = 'completedChallengeStreak',
-    completedTeamMemberStreak = 'completedTeamMemberStreak',
-    createdSoloStreak = 'createdSoloStreak',
-    createdTeamStreak = 'createdTeamStreak',
-    joinedChallenge = 'joinedChallenge',
-    followedUser = 'followedUser',
-    subscribedToAnnualPlan = 'subscribedToAnnualPlan',
-    subscribedToMonthlyPlan = 'subscribedToMonthlyPlan',
+    registered = 'Registered',
+    completedSoloStreak = 'Completed Solo Streak',
+    completedChallengeStreak = 'Completed Challenge Streak',
+    completedTeamMemberStreak = 'Completed Team Member Streak',
+    createdSoloStreak = 'Created Solo Streak',
+    createdTeamStreak = 'Created Team Streak',
+    joinedChallenge = 'Joined Challenge',
+    followedUser = 'Followed User',
+    subscribedToAnnualPlan = 'Subscribed To Annual Plan',
+    subscribedToMonthlyPlan = 'Subscribed To Monthly Plan',
 }
 
 export default AnalyticEventsTypes;
