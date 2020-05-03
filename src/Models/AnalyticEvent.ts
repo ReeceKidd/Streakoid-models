daf663d347aaca46f9e62a96df8f2b6c6ebe8909
Reece Kidd <reecekidd95@gmail.com>
2020-05-03 11:37:02 +0100

    refactor: Removed unnecessary userId and usernames for AnalyticEvent

--- src/Models/AnalyticEvent.ts
+++ src/Models/AnalyticEvent.ts
@@ -16,64 +16,41 @@ export interface CompletedSoloStreak {
 }
 
 export interface CompletedChallengeStreak {
-    userId: string;
-    username: string;
     challengeStreakId: string;
     challengeId: string;
     challengeName: string;
     currentStreakNumberOfDaysInARow: number;
     totalTimesTracked: number;
-    createdAt: string;
 }
 
 export interface CompletedTeamMemberStreak {
-    userId: string;
-    username: string;
     teamMemberStreakId: string;
     teamStreakId: string;
     teamStreakName: string;
     members: BasicUser[];
     currentStreakNumberOfDaysInARow: number;
     totalTimesTracked: number;
-    createdAt: string;
 }
 
 export interface CreatedSoloStreak {
-    userId: string;
-    username: string;
     soloStreakId: string;
     soloStreakName: string;
+    soloStreakDescription: string;
 }
 
 export interface CreatedTeamStreak {
-    userId: string;
-    username: string;
     teamStreakId: string;
     teamStreakName: string;
     members: BasicUser[];
 }
 
 export interface JoinedChallenge {
-    userId: string;
-    username: string;
     challengeId: string;
     challengeName: string;
     numberOfMembersInChallenge: number;
 }
 
 export interface FollowedUser {
-    userId: string;
-    username: string;
     userFollowedId: string;
     userFollowedUsername: string;
-}
-
-export interface SubscribedToAnnualPlan {
-    userId: string;
-    username: string;
-}
-
-export interface SubscribedToMonthlyPlan {
-    userId: string;
-    username: string;
 }
