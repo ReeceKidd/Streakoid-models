433f9d0d6d117211bafb255a0eade086b26def90
Reece Kidd <reecekidd95@gmail.com>
2020-06-01 17:29:52 +0100

    refactor: Added cognitoUsername to User model

--- src/Models/User.ts
+++ src/Models/User.ts
@@ -35,6 +35,7 @@ export interface User {
     createdAt: string;
     updatedAt: string;
     username: string;
+    cognitoUsername: string;
     temporaryPassword: string;
     email?: string;
     name?: string;
