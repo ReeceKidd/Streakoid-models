989593ef2776c9936ac9e49d9104ed46a66f1c6b
Reece Kidd <reecekidd95@gmail.com>
2020-06-18 13:58:50 +0100

    refactor: Added OneHundredDayChallengeStreakAchievement to AchievementType

--- src/Models/Achievement.ts
+++ src/Models/Achievement.ts
@@ -1,9 +1,15 @@
 import AchievementTypes from '../Types/AchievementTypes';
 
-export type AchievementType = OneHundredDaySoloStreakAchievement;
+export type AchievementType = OneHundredDaySoloStreakAchievement | OneHundredDayChallengeStreakAchievement;
 
 export interface OneHundredDaySoloStreakAchievement {
     achievementType: AchievementTypes.oneHundredDaySoloStreak;
+    name: string;
+    description: string;
+}
+
+export interface OneHundredDayChallengeStreakAchievement {
+    achievementType: AchievementTypes.oneHundredDayChallengeStreak;
     name: string;
     description: string;
 }
