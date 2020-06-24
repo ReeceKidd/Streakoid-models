5b56e880ce1e816098556da89f20949e7eff4b8f
Reece Kidd <reecekidd95@gmail.com>
2020-06-24 13:12:21 +0100

    refactor: Removed deviceType and endpointArn from PushNotification

--- src/Models/PushNotification.ts
+++ src/Models/PushNotification.ts
@@ -1,10 +1,5 @@
-import PushNotificationSupportedDeviceTypes from '../Types/PushNotificationSupportedDeviceTypes';
-
 export interface PushNotification {
     token: string;
-    deviceType: PushNotificationSupportedDeviceTypes;
     androidEndpointArn?: string;
     iosEndpointArn?: string;
-    //Legacy support
-    endpointArn?: string;
 }
