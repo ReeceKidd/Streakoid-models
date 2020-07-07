ddb36acbbd265c07dafb703a0d9b7e32728af84b
Reece Kidd <reecekidd95@gmail.com>
2020-07-07 16:58:01 +0100

    refactor: Added new keys to PushNotificationRecord

--- src/Models/PushNotificationRecord.ts
+++ src/Models/PushNotificationRecord.ts
@@ -3,6 +3,10 @@ import PushNotificationTypes from '../Types/PushNotificationTypes';
 export interface PushNotificationRecord {
     userId: string;
     pushNotificationType: PushNotificationTypes;
+    title: string;
+    body: string;
+    androidEndpointArn?: string;
+    iosEndpointArn?: string;
     createdAt: string;
     updatedAt: string;
 }
