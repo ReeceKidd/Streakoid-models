719862e2fcf9763e1b8a287e6c1b6fa5e56423b2
Reece Kidd <reecekidd95@gmail.com>
2020-08-11 09:09:13 +0100

    refacor: Updated LongestCurrentStreak so that it doesn't include a LongestTeamStreak type and it can equal an empty object

--- src/Models/LongestCurrentStreak.ts
+++ src/Models/LongestCurrentStreak.ts
@@ -1,13 +1,5 @@
 import { LongestSoloStreak } from './LongestSoloStreak';
 import { LongestChallengeStreak } from './LongestChallengeStreak';
 import { LongestTeamMemberStreak } from './LongestTeamMemberStreak';
-import { LongestTeamStreak } from './LongestTeamStreak';
 
-export type LongestCurrentStreak =
-    | LongestSoloStreak
-    | LongestChallengeStreak
-    | LongestTeamMemberStreak
-    | LongestTeamStreak
-    | {
-          numberOfDays: number;
-      };
+export type LongestCurrentStreak = LongestSoloStreak | LongestChallengeStreak | LongestTeamMemberStreak | {};
