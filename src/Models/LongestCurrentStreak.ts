0a81bd5b972172d1ad8042182be6ca48ed38447b
Reece Kidd <reecekidd95@gmail.com>
2020-08-11 09:37:24 +0100

    refactor: Readded numberOfDays to LongestCurrentStreak

--- src/Models/LongestCurrentStreak.ts
+++ src/Models/LongestCurrentStreak.ts
@@ -2,4 +2,10 @@ import { LongestSoloStreak } from './LongestSoloStreak';
 import { LongestChallengeStreak } from './LongestChallengeStreak';
 import { LongestTeamMemberStreak } from './LongestTeamMemberStreak';
 
-export type LongestCurrentStreak = LongestSoloStreak | LongestChallengeStreak | LongestTeamMemberStreak | {};
+export type LongestCurrentStreak =
+    | LongestSoloStreak
+    | LongestChallengeStreak
+    | LongestTeamMemberStreak
+    | {
+          numberOfDays: number;
+      };
